2bd471d5121a35acb87e3db8aaddfda4691c8f43
Khang Nguyen Phuc <npkhang721@gmail.com>
2023-08-02 16:02:48 +0700

    first commit

---
+++ YOLO_detect.py
@@ -0,0 +1,46 @@
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import cv2
+import numpy as np
+# from telegram_utils import send_telegram
+import datetime
+import threading
+import torch
+
+def isInside(points, centroid):
+    polygon = Polygon(points)
+    centroid = Point(centroid)
+    return polygon.contains(centroid)
+
+def draw_bbox(frame, xmin, ymin, xmax, ymax, confidence, class_name):
+    start_point = (xmin, ymax)
+    end_point = (xmax, ymin)
+    color = (255, 0, 0)
+    result = cv2.rectangle(frame, np.int32(start_point), np.int32(end_point), color, 2)
+    result = cv2.putText(frame, str(confidence), (int(np.mean([xmin, xmax])), int(ymin + 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+    return result
+
+class YOLO():
+    def __init__(self):
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+    def detect(self, frame, points):
+        result = self.model(frame)
+        try:
+            for obj in np.array(result.xyxy[0]):
+                xmin, ymin, xmax, ymax, confidence, class_name = obj
+                if int(class_name) == 0:
+                    frame = draw_bbox(frame, xmin, ymin, xmax, ymax, confidence, class_name)
+                    # Tinh toan centroid
+                    centroid = (int(np.mean([xmin, xmax])), int(np.mean([ymin, ymax])))
+                    cv2.circle(frame, centroid, 5, (0, 0, 255), -1)
+
+                    if isInside(points, centroid):
+                        frame = self.alert(frame)
+            return frame
+        except:
+            pass
+    
+    def alert(self, img):
+        cv2.putText(img, "ALARM!!!!", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        return img
---
+++ main.py
@@ -0,0 +1,57 @@
+import cv2
+import numpy as np
+from imutils.video import VideoStream
+import torch
+from YOLO_detect import YOLO
+
+# List points of polygon
+points = []
+
+def handle_left_click(event, x, y, flags, points):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        points.append([x, y])
+
+def draw_polygon(frame, points):
+    for point in points:
+        frame = cv2.circle(img=frame, 
+                           center=(point[0], point[1]), 
+                           radius=5, 
+                           color=(0,0,255))
+    frame = cv2.polylines(frame, [np.int32(points)], False, (255, 0, 0), 2)
+    return frame
+
+# Camera
+video = VideoStream(src=0).start()
+
+# Model
+model = YOLO()
+detect  = False
+
+while True:
+    # Read frame
+    frame = video.read()
+    frame = cv2.flip(frame, 1)
+
+    # Draw polygon
+    frame = draw_polygon(frame, points)
+
+    # Detect decision
+    if detect:
+        try:
+            frame = model.detect(frame, points)
+        except:
+            pass
+
+    key = cv2.waitKey(1)
+    if key == ord('q'): # Quit
+        break
+    elif key == ord('d'): # Close polygon
+        points.append(points[0])
+        detect = True
+
+    # Show frame
+    cv2.imshow('Intrusion Warning', frame)
+    cv2.setMouseCallback('Intrusion Warning', handle_left_click, points)
+
+video.stop()
+cv2.destroyAllWindows()
